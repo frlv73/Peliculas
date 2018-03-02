eaaa18f06d0007e7cde373a6282e469db21f554b
frlv73 <leovilla91@gmail.com>
2018-03-02 07:52:40 -0300

    Add project files.

---
+++ MVCPeliculas/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPeliculas
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MVCPeliculas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCPeliculas.MvcApplication" Language="C#" %>
